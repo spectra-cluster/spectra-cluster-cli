53346907376d36046573cc0ad90d336b0d83849a
Johannes Griss <jgriss@users.noreply.github.com>
2015-05-16 22:04:26 +0200

    Completed SpectrumWriter.

--- src/main/java/uk/ac/ebi/pride/spectracluster/spectra_list/SpectrumWriter.java
+++ src/main/java/uk/ac/ebi/pride/spectracluster/spectra_list/SpectrumWriter.java
@@ -1,21 +1,30 @@
 package uk.ac.ebi.pride.spectracluster.spectra_list;
 
+import uk.ac.ebi.pride.spectracluster.io.BinaryClusterAppender;
+import uk.ac.ebi.pride.spectracluster.spectrum.ISpectrum;
+import uk.ac.ebi.pride.spectracluster.util.ClusterUtilities;
+import uk.ac.ebi.pride.spectracluster.util.Defaults;
+import uk.ac.ebi.pride.spectracluster.util.SpectrumConverter;
+import uk.ac.ebi.pride.spectracluster.util.SpectrumUtilities;
+import uk.ac.ebi.pride.spectracluster.util.function.IFunction;
 import uk.ac.ebi.pride.tools.jmzreader.JMzReader;
+import uk.ac.ebi.pride.tools.jmzreader.model.Spectrum;
+import uk.ac.ebi.pride.tools.mgf_parser.MgfFile;
 
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
+ * Writes spectra stored as SpectrumReferences to a file.
+ *
  * Created by jg on 13.05.15.
  */
 public class SpectrumWriter {
     private final File outputFile;
+    protected static final IFunction<ISpectrum, ISpectrum> filterFunction = Defaults.getDefaultPeakFilter();
 
     public SpectrumWriter(File outputFile) {
         this.outputFile = outputFile;
@@ -30,7 +39,39 @@ public class SpectrumWriter {
 
         // open the file and write to it
         FileOutputStream outputStream = new FileOutputStream(outputFile);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
 
+        for (SpectrumReference spectrumReference : spectrumReferences) {
+            int fileIndex = spectrumReference.getFileId();
 
+            if (fileIndex >= peakListFilenames.length)
+                throw new Exception("Invalid file id for spectrum reference");
+
+            if (!readerPerFileIndex.containsKey(fileIndex))
+                readerPerFileIndex.put(fileIndex, openFile(peakListFilenames[fileIndex]));
+
+            JMzReader fileReader = readerPerFileIndex.get(fileIndex);
+
+            // load the spectrum
+            Spectrum spectrum = fileReader.getSpectrumByIndex(spectrumReference.getSpectrumIndex());
+
+            // pre-process the spectrum
+            ISpectrum processedSpectrum = filterFunction.apply(SpectrumConverter.convertJmzReaderSpectrum(spectrum));
+
+            // write it to the file
+            BinaryClusterAppender.INSTANCE.appendCluster(objectOutputStream, ClusterUtilities.asCluster(processedSpectrum));
+        }
+
+        // close the file
+        BinaryClusterAppender.INSTANCE.appendEnd(objectOutputStream);
+        objectOutputStream.close();
+        outputStream.close();
+    }
+
+    private JMzReader openFile(String peakListFilename) throws Exception {
+        if (peakListFilename.toLowerCase().endsWith(".mgf"))
+            return new MgfFile(new File(peakListFilename));
+
+        throw new Exception("Unknown file extension encountered: " + peakListFilename);
     }
 }
