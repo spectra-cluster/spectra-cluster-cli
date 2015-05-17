3294efc9d348f6dc1834c7448fdec044f6432b9d
Johannes Griss <jgriss@users.noreply.github.com>
2015-05-17 10:56:51 +0200

    Added spectrum title as property to loaded spectra if available.

--- src/main/java/uk/ac/ebi/pride/spectracluster/util/SpectrumConverter.java
+++ src/main/java/uk/ac/ebi/pride/spectracluster/util/SpectrumConverter.java
@@ -2,11 +2,15 @@ package uk.ac.ebi.pride.spectracluster.util;
 
 import uk.ac.ebi.pride.spectracluster.spectrum.IPeak;
 import uk.ac.ebi.pride.spectracluster.spectrum.ISpectrum;
+import uk.ac.ebi.pride.spectracluster.spectrum.KnownProperties;
 import uk.ac.ebi.pride.spectracluster.spectrum.Peak;
+import uk.ac.ebi.pride.tools.jmzreader.model.Param;
 import uk.ac.ebi.pride.tools.jmzreader.model.Spectrum;
+import uk.ac.ebi.pride.tools.jmzreader.model.impl.CvParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by jg on 13.05.15.
@@ -26,7 +30,16 @@ public final class SpectrumConverter {
         }
 
         // create the spectrum
-        ISpectrum convertedSpectrum = new uk.ac.ebi.pride.spectracluster.spectrum.Spectrum(jmzReaderSpectrum.getId(), jmzReaderSpectrum.getPrecursorCharge(), (float) jmzReaderSpectrum.getPrecursorMZ().doubleValue(), Defaults.getDefaultQualityScorer(), peaks);
+        String uniqueId = UUID.randomUUID().toString();
+        ISpectrum convertedSpectrum = new uk.ac.ebi.pride.spectracluster.spectrum.Spectrum(uniqueId, jmzReaderSpectrum.getPrecursorCharge(), (float) jmzReaderSpectrum.getPrecursorMZ().doubleValue(), Defaults.getDefaultQualityScorer(), peaks);
+
+        // set the original title if available
+        for (CvParam param : jmzReaderSpectrum.getAdditional().getCvParams()) {
+            if ("MS:1000796".equals(param.getAccession())) {
+                convertedSpectrum.setProperty(KnownProperties.SPECTRUM_TITLE, param.getValue());
+                break;
+            }
+        }
 
         return convertedSpectrum;
     }
